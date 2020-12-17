90bd6331936bc80310c9e4edcd17ac88a67c5630
udit2006 <74304949+udit2006@users.noreply.github.com>
2020-12-17 17:59:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3},{isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -53,13 +53,10 @@ function draw() {
   packageSprite.x= packageBody.position.x; 
   packageSprite.y= packageBody.position.y;
   
-  if(keyPressed,DOWN_ARROW)
-  {
-	Matter.Body.setStatic(packageBody,false); 
-  }
+  
   drawSprites();
 
-  keyPressed();
+ 
  
 }
 
